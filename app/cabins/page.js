0d98886cd53e61230e0b694ee5b0969092318637
Novita H <novitahamilton@gmail.com>
2025-11-07 21:53:28 -0800

    Initial commit

---
+++ app/cabins/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+
+    return (
+        <div>
+            <h1>Cabins page</h1>
+        </div>
+    );
+}
+
+
---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import Navigation from "@/app/_components/Navigation";
+import Logo from "@/app/_components/Logo";
+import "@/app/_styles/globals.css";
+
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
+
+function RootLayout( { children }) {
+  return (
+    <html lang="en">
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
+        <header>
+        <Logo />
+        <Navigation />
+
+        </header>
+       
+        <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
+      </body>
+
+    </html>
+  );
+
+}
+
+export default RootLayout;
