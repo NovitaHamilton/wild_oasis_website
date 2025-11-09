76158c6e90f2c06e89c9f551a9fc30514678fa61
Novita H <novitahamilton@gmail.com>
2025-11-08 19:14:42 -0800

    Set up font and Fix typo in metadata > description

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,27 @@
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans} from "next/font/google";
+
+const josefin = Josefin_Sans( {
+  subsets: ["latin"],
+  display: "swap",
+})
+
 
 export const metadata = {
   title: {
     template: "%s | The Wild Oasis",
     default: "The Wild Oasis",
   },
-  description: "Luxurious cabin hotel, located in the heart of the Canadian Rockies, sourrounded by rugged mountains and crystal clear lakes.",
+  description: "Luxurious cabin hotel, located in the heart of the Canadian Rockies, surrounded by beautiful rugged mountains and crystal-clear lakes.",
 };
 
 
 function RootLayout( { children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
         <Logo />
         <Navigation />
